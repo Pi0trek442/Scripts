38464903e742a699d4e203191e20dacc960861ab
Pi0trek442 <121568050+Pi0trek442@users.noreply.github.com>
2024-08-12 11:56:09 +0200

    Update ImportWifiProfiles.ps1

--- Microsoft Windows/ImportWifiProfiles.ps1
+++ Microsoft Windows/ImportWifiProfiles.ps1
@@ -1,1 +1,8 @@
+# Set your path to the folder containing *.xml
+$folderPath = "C:\xxxx\rrrrr\"
 
+# Read all *.xml files in a folder
+foreach ($file in Get-ChildItem -Path $folderPath -Filter *.xml) {
+    # The command to import profile
+    Invoke-Expression "netsh wlan add profile filename='$($file.FullName)' user=all"
+}
