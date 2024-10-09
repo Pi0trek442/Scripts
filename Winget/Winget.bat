d9d0fa5cb0acfe7af86a62e4d82c56eda5116bcb
Pi0trek442 <121568050+Pi0trek442@users.noreply.github.com>
2024-10-09 16:19:57 +0200

    Create Winget.bat
    Script pour installer les logiciels de bas pour une école

---
+++ Winget/Winget.bat
@@ -0,0 +1,24 @@
+@echo off
+Echo install Logiciels Ecoles
+REM install firefox
+winget install Mozilla.Firefox.ESR.fr
+REM install Teamviewer
+winget install TeamViewer.TeamViewer
+REM install Adobe Reader
+winget install Adobe.Acrobat.Reader.64-bit
+REM Install LibreOffice
+winget install TheDocumentFoundation.LibreOffice
+REM Install Google Chrome
+winget install Google.Chrome
+REM install Greenshot
+winget install Greenshot.Greenshot
+REM install 
+winget install Google.EarthPro
+REM install 
+winget install Audacity.Audacity
+REM install 
+winget install VideoLAN.VLC
+REM install 
+winget install KDE.Krita
+REM install 
+winget install GIMP.GIMP
